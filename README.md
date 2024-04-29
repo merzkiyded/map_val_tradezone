b685fd32ac919e26121cf8ee99be741c3c884c57
merzkiyded <167827177+merzkiyded@users.noreply.github.com>
2024-04-30 01:14:40 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# map_val_tradezone
+
+
+
+Other jobs in Discord: https://discord.gg/wF826eSUPD
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+this_is_a_map "yes"
+dependency 'objectloader'
+
+files {
+	'val_trade.xml',
+	'stream/*.ymap',
+}
+
+objectloader_maps {
+	'val_trade.xml',
+}
+
+author 'Merzkiy_ded'
